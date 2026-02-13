e12d3edc5a5dcfa7b03a22ad3bdca98414316104
Mauricio Gavilan Aguiar <mgavilan@fgs.co>
2026-02-13 00:10:05 -0500

    update frontend submodule

--- fgs-identity-management/src/main.ts
+++ fgs-identity-management/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
